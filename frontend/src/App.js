0a9a063098f61b9edadb2c2301a583eb04b90705
CapSap <cshotam@gmail.com>
2022-01-30 10:59:41 +1100

    front tailwind setup

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+
+function App() {
+  return <div className="text-3xl font-bold underline">App comp as</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
